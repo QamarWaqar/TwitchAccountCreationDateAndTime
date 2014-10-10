cac4a0617dc386392968e90be775c96c936733f4
QamarWaqar <qamogle@gmail.com>
2014-10-10 11:50:26 +0500

    Added Exception Handling for "User Not Found"
    The server returned and error "The server returned and error 404 not
    found" and the application crashed, but fixed it to not to crash but ask
    the user "Do another?" Handled Application crash.

--- TwitchAccountCreationDate/Program.cs
+++ TwitchAccountCreationDate/Program.cs
@@ -36,65 +36,70 @@ namespace TwitchAccountCreationDate
             Console.WriteLine("Please input the Username: ");
             string input = Console.ReadLine();
             url.Append(input);
-            string str = wC.DownloadString(url.ToString());
-            //Console.WriteLine(str);
-
-            int keepingTrack = 0;
-            int skip4Commas = 0;
-            bool done = false;
-            StringBuilder sB = new StringBuilder();
-            foreach (var v in str)
+            try
             {
-                keepingTrack++;
-                if (v == ',') { skip4Commas++; }
-                if (skip4Commas == 4)
+                string str = wC.DownloadString(url.ToString());
+                //Console.WriteLine(str);
+
+                int keepingTrack = 0;
+                int skip4Commas = 0;
+                bool done = false;
+                StringBuilder sB = new StringBuilder();
+                foreach (var v in str)
                 {
-                    for (int i = keepingTrack; i <= 50000; i++)
+                    keepingTrack++;
+                    if (v == ',') { skip4Commas++; }
+                    if (skip4Commas == 4)
                     {
-                        if (str[i] == '"' || str[i] == ':') { } else { sB.Append(str[i]); }
-                        if (str[i] == ',') { done = true; break; }
+                        for (int i = keepingTrack; i <= 50000; i++)
+                        {
+                            if (str[i] == '"' || str[i] == ':') { } else { sB.Append(str[i]); }
+                            if (str[i] == ',') { done = true; break; }
+                        }
+                    }
+                    if (done == true) { break; }
+                }
+                Console.WriteLine("The Account was Created On Date: ");
+                StringBuilder cD = new StringBuilder();
+                for (int i = 0; i <= sB.Length; i++)
+                {
+                    if (i >= 10)
+                    {
+                        cD.Append(sB[i]);
+                        if (sB[i] == 'T')
+                        { break; }
                     }
                 }
-                if (done == true) { break; }
-            }
-            Console.WriteLine("The Account was Created On Date: ");
-            StringBuilder cD = new StringBuilder();
-            for (int i = 0; i <= sB.Length; i++)
-            {
-                if (i >= 10)
-                {
-                    cD.Append(sB[i]);
-                    if (sB[i] == 'T')
-                    { break; }
-                }
-            }
-            cD.Remove(cD.Length - 1, 1);
-            Console.WriteLine(cD);
+                cD.Remove(cD.Length - 1, 1);
+                Console.WriteLine(cD);
 
-            Console.WriteLine("And the Time was: ");
-            StringBuilder cT = new StringBuilder();
-            bool donedone = false;
-            for (int i = 0; i <= sB.Length; i++)
-            {
-                if (sB[i] == 'T')
+                Console.WriteLine("And the Time was: ");
+                StringBuilder cT = new StringBuilder();
+                bool donedone = false;
+                for (int i = 0; i <= sB.Length; i++)
                 {
-                    for (int j = i; j <= sB.Length - 1; j++)
-                    { cT.Append(sB[j]); }
-                    donedone = true; break;
+                    if (sB[i] == 'T')
+                    {
+                        for (int j = i; j <= sB.Length - 1; j++)
+                        { cT.Append(sB[j]); }
+                        donedone = true; break;
+                    }
+                    if (donedone == true) { break; }
                 }
-                if (donedone == true) { break; }
+                cT.Remove(cT.Length - 2, 2);
+                cT.Remove(0, 1);
+                int dotdot = 0;
+                foreach (var v in cT.ToString())
+                {
+                    dotdot++;
+                    if (dotdot == 1 || dotdot == 3 || dotdot == 5)
+                    { Console.Write(':'); }
+                    Console.Write(v);
+                }
+                Console.Write('\n');
             }
-            cT.Remove(cT.Length - 2, 2);
-            cT.Remove(0, 1);
-            int dotdot = 0;
-            foreach (var v in cT.ToString())
-            {
-                dotdot++;
-                if (dotdot == 1 || dotdot == 3 || dotdot == 5)
-                { Console.Write(':'); }
-                Console.Write(v);
-            }
-            Console.Write('\n');
+            catch (System.Exception e)
+            { Console.WriteLine("User Does Not Exist On Twitch.tv"); }
         }
     }
 }
